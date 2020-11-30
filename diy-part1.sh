06af15887f42648deba27eccb9a63f93ab68510a
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-12-01 07:25:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -30,5 +30,4 @@ sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.co
 
 #add openwrt-packages
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
